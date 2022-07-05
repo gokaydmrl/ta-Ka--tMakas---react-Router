92fd7f62c955f441e62e41d1acb008ff24406a35
gokaydmrl <gokaydemirel96@gmail.com>
2022-07-05 14:37:49 +0300

    master

--- src/Oyun.jsx
+++ src/Oyun.jsx
@@ -8,17 +8,14 @@ const List = {
   taş: <FaHandRock />,
   kağıt: <FaHandPaper />,
   makas: <FaHandScissors />,
-  Liste: ["taş", "kağıt", "makas", "taş", "kağıt", "makas"],
 };
 
-let scores = {
-  score1: 0,
-  score2: 0,
-};
+const Liste = ["taş", "kağıt", "makas", "taş", "kağıt", "makas"];
 
 const rndmSayı = () => {
   return Math.floor(Math.random() * 5 + 1);
 };
+
 
 const Oyun = () => {
   const [game, setGame] = useState({
@@ -27,6 +24,8 @@ const Oyun = () => {
     result: "sonuç bekleniyor",
     set: "11'de biter",
     winner: "",
+    score1: 0,
+    score2: 0,
   });
 
   const navigate = useNavigate();
@@ -34,13 +33,15 @@ const Oyun = () => {
   const handleClick = () => {
     // kurnazlık yaptık ama daha iyi bir yol elbet vardır
     const newGame = {
-      player1: List[List.Liste[rndmSayı()]],
-      player2: List[List.Liste[rndmSayı()]],
+      player1: List[Liste[rndmSayı()]],
+      player2: List[Liste[rndmSayı()]],
     };
 
     let result = game.result;
     let set = game.set;
     let winner = game.winner;
+    let score1 = game.score1;
+    let score2 = game.score2;
     // oyunlar için if else
     // 1'in kazandığı durumda score1'i 1 artırıyorum. player2 kazanınca score2yi artırıyorum,
     // daha sonradan toplamı alıyorum
@@ -48,86 +49,88 @@ const Oyun = () => {
     ////////////
 
     if (newGame.player1 === List.kağıt && newGame.player2 === List.taş) {
-      scores.score1++;
+      score1++;
       winner = "player 1";
       result = `${winner} kazandı`;
     } else if (
       newGame.player1 === List.kağıt &&
       newGame.player2 === List.makas
     ) {
-      scores.score2++;
+      score2++;
       winner = "player 2";
       result = `${winner} kazandı`;
     } else if (newGame.player1 === List.taş && newGame.player2 === List.kağıt) {
-      scores.score2++;
+      score2++;
       winner = "player 2";
       result = `${winner} kazandı`;
     } else if (newGame.player1 === List.taş && newGame.player2 === List.makas) {
-      scores.score1++;
+      score1++;
       winner = "player 1";
       result = `${winner} kazandı`;
     } else if (
       newGame.player1 === List.makas &&
       newGame.player2 === List.kağıt
     ) {
-      scores.score1++;
+      score1++;
       winner = "player 1";
       result = `${winner} kazandı`;
     } else if (newGame.player1 === List.makas && newGame.player2 === List.taş) {
-      scores.score2++;
-      result = "player 2 kazandı";
+      score2++;
+      winner = "player 2";
+      result = `${winner}kazandı`;
     } else {
       winner = "dostluk";
       result = `${winner} kazandı :)`;
     }
 
-    console.log(scores.score1, scores.score2);
+    console.log(score1, score2);
 
     ///////////
     // set için toplam 11 oyunun kazananı
     // scoresların toplamı 11se set bitmiştir demek
     // beraberlik durumunda artış yok
 
-    if (scores.score1 + scores.score2 === 11)
-      if (scores.score1 > scores.score2) {
+    if (score1 + score2 === 11)
+      if (score1 > score2) {
         winner = "player 1";
-        set = `setin galibi ${wiinner} skor: ${scores.score1} - ${scores.score2}`;
+        set = `setin galibi ${wiinner} skor: ${score1} - ${score2}`;
       } else {
         winner = "player 2";
-        set = `setin galibi ${winner} skor: ${scores.score2} - ${scores.score1}`;
+        set = `setin galibi ${winner} skor: ${score2} - ${score1}`;
       }
-    return setGame({ ...newGame, result, set, winner });
+    return setGame({ ...newGame, result, set, winner, score1, score2 });
   };
 
   useEffect(() => {
     const timer = () => {
       setTimeout(() => {
-        scores = { score1: 0, score2: 0 };
         navigate("/users", { replace: true });
         alert(
           `seti ${game.winner} kazandı. users sayfasına yönlendiriliyorsunuz`
         );
+        setGame({
+          score1: 0,
+          score2: 0,
+        });
       }, 2000);
     };
-
-    scores.score1 + scores.score2 === 11 && timer();
+    game.score1 + game.score2 === 11 && timer();
 
     return () => {
       clearTimeout(timer);
     };
-  }, [scores.score1, scores.score2]);
+  }, [game.score1, game.score2]);
 
   return (
     <div>
       <p>
-        
         Player 1<span className="oyun"> {game.player1}</span>
         <strong>VS</strong> Player 2
         <span className="oyun"> {game.player2}</span>
       </p>
       <br />
       <button
-        disabled={scores.score1 + scores.score2 === 11 ? true : false}
+        disabled={game.score1 + game.score2 === 11 ? true : false}
         onClick={handleClick}
       >
         sonucu gör
